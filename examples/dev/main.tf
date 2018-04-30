9a168a893098e0ca46a4908a1909b23810301682
Jon Benson <bensojon@gmail.com>
2018-04-30 19:47:50 +1000

    Remove f-refactor branch

--- examples/dev/main.tf
+++ examples/dev/main.tf
@@ -19,7 +19,7 @@ data "aws_ami" "base" {
 }
 
 module "network_aws" {
-  # source = "github.com/hashicorp-modules/network-aws?ref=f-refactor"
+  # source = "github.com/hashicorp-modules/network-aws"
   source = "../../../network-aws"
 
   name              = "${var.name}"
@@ -32,7 +32,7 @@ module "network_aws" {
 }
 
 module "vault_lb_aws" {
-  # source = "github.com/hashicorp-modules/vault-lb-aws?ref=f-refactor"
+  # source = "github.com/hashicorp-modules/vault-lb-aws"
   source = "../../../vault-lb-aws"
 
   name           = "${var.name}"
--- examples/no-provision/main.tf
+++ examples/no-provision/main.tf
@@ -1,5 +1,5 @@
 module "vault_lb_aws" {
-  # source = "github.com/hashicorp-modules/vault-lb-aws?ref=f-refactor"
+  # source = "github.com/hashicorp-modules/vault-lb-aws"
   source = "../../../vault-lb-aws"
 
   create      = false
